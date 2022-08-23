4590dc98dd1aff4039baca99a6f7f84a7a570f54
Zuleika Tesei <zuleikatesei@zuleikas-mbp.lan>
2022-08-22 17:18:14 -0700

    dynamically create html page in node

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('/index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output.');
+});
