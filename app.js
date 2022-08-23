d2f6b7cfb422b954132ecc82ace978323ae6d9c2
Zuleika Tesei <zuleikatesei@zuleikas-mbp.lan>
2022-08-23 16:01:30 -0700

    installed and tested inquirer

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
-console.log(profileDataArgs);
+inquirer 
+    .prompt ([
+        {
+            type: 'input',
+            name: 'Name',
+            message: 'What is your name?'
+        }
+    ])
+    .then (answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
-console.log(name, github);
-
-const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage(name, github);
 
 
-fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output.');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output.');
+// });
