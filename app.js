d4898ab7be41a900e4f7fb2eafba2cc3403e577f
Zuleika Tesei <zuleikatesei@zuleikas-mbp.lan>
2022-08-23 17:55:47 -0700

    called project question promptwith ability to list multiple projects

--- app.js
+++ app.js
@@ -1,14 +1,86 @@
 const inquirer = require('inquirer');
 
-inquirer 
-    .prompt ([
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'Name',
             message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'Github:',
+            message: 'Enter your GitHub Username:'
+        },
+        {
+            type: 'input', 
+            name: 'About:',
+            message: 'Provide some information about yourself.'
+        }
+    ]);
+};
+
+// promptUser().then (answers => console.log(answers));
+
+const promptProject = (portfolioData) => {
+    
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+    portfolioData.projects = [];
+    }
+
+    console.log (`
+    =================
+    Add a New Project
+    ================= 
+    `);
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Name:',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'Description:',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'Languages:',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'Link:',
+            message: 'Enter the Github link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
         }
     ])
-    .then (answers => console.log(answers));
+
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData);
+        } else {
+          return portfolioData;
+        }
+      });
+};
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
+   
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
 
