61f6207febf67cce63492d13d203c6482ca007a0
Zuleika Tesei <zuleikatesei@zuleikas-mbp.lan>
2022-08-29 09:06:12 -0700

    Handle Node.js Asynchronicity

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
 const inquirer = require("inquirer");
+const { writeFile, copyFile } = require("./utils/generate-site");
 const generatePage = require("./src/page-template");
 
 const promptUser = () => {
@@ -138,13 +138,18 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile("./index.html", pageHTML, err => {
-      if (err) throw err;
-
-      console.log(
-        "Portfolio complete! Check out index.html to see the output."
-      );
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
